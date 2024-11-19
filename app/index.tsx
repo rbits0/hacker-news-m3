6b3209e3174a86cc4b461ad2e7e69c644e0e885d
rbits0 <rbits0@gmail.com>
2024-11-19 14:21:16 +1100

    Install react-native-paper

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import { View } from "react-native";
+import { Text, Button } from "react-native-paper";
 
 export default function Index() {
   return (
@@ -10,6 +11,9 @@ export default function Index() {
       }}
     >
       <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Button mode="contained">
+        Hello
+      </Button>
     </View>
   );
 }
