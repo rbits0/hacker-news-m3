da7318a12e05fb1833be2f0daf8cc9a21b4c82f2
rbits0 <rbits0@gmail.com>
2025-01-29 17:32:40 +1100

    Load items as you scroll

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,10 +2,13 @@ import Story from '@/components/Story';
 import { RootState } from '@/store';
 import Item from '@/store/Item';
 import { StoriesType, useGetFrontPageIdsByStoriesTypeQuery, useGetItemByIdQuery } from '@/store/services/hackerNews';
-import { useMemo } from 'react';
-import { FlatList, StyleSheet, View } from 'react-native';
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+import { FlatList, StyleSheet, View, ViewToken } from 'react-native';
 import { Text, Button, useTheme } from 'react-native-paper';
 import { useSelector } from 'react-redux';
+
+
+const NUM_STORIES_PER_PAGE = 20;
 
 
 export default function Index() {
@@ -17,24 +20,39 @@ export default function Index() {
     isLoading: itemsAreLoading,
   } = useGetFrontPageIdsByStoriesTypeQuery(StoriesType.Top);
 
-  const {
-    data: testItem,
-    error: testItemError,
-    isLoading: testItemIsLoading,
-  } = useGetItemByIdQuery(42276865);
+  // Only load a certain number of stories at a time
+  const [numStories, setNumStories] = useState(NUM_STORIES_PER_PAGE);
+  const itemIdsToRender = itemIds?.slice(0, numStories);
 
 
-  const testItems = [testItem];
+  // useCallback with empty dependency list, since FlatList doesn't like functions changing
+  const onViewableItemsChanged = useCallback(({ changed, viewableItems }: {
+    changed: ViewToken<number>[],
+    viewableItems: ViewToken<number>[],
+  }) => {
+    // Since we're in a useCallback, we don't have access to the current numStories
+    // Run the code inside the setNumStories callback instead, so we have the current numStories
+    setNumStories(numStories => {
+      let maxIndex = Math.max(...viewableItems.map(viewToken => viewToken.index!));
+
+      // If less than a page away, load new stories
+      if (maxIndex > numStories - NUM_STORIES_PER_PAGE) {
+        return numStories + NUM_STORIES_PER_PAGE;
+      } else {
+        return numStories;
+      }
+    });
+  }, []);
+
 
   return (
     <View
       style={[styles.container, { backgroundColor: theme.colors.background }]}
     >
-      <Story item={testItem} />
-
       <FlatList
-        data={itemIds}
+        data={itemIdsToRender}
         renderItem={({ item }) => <Story itemId={item} />}
+        onViewableItemsChanged={onViewableItemsChanged}
         contentContainerStyle={styles.innerList}
         style={styles.list}
       />
