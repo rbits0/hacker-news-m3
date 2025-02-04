13bebbd8acfa676d9edb7669da61196bb1131df9
rbits0 <rbits0@gmail.com>
2025-02-04 15:45:28 +1100

    Create greasemonkey script for hacker news CORS requests

---
+++ greasemonkey/hacker-news-m3-cors.js
@@ -0,0 +1,35 @@
+// ==UserScript==
+// @name         hacker-news-m3 CORS
+// @namespace    rbits.hacker-news-m3-cors
+// @description  Allows Hacker News CORS requests for hacker-news-m3
+// @version      1.0
+// @author       rbits
+// @icon         https://news.ycombinator.com/y18.svg
+// @match        *://localhost*
+// @match        file:///*
+// @connect      news.ycombinator.com
+// @grant        GM.xmlHttpRequest
+// @license      GPL3
+// ==/UserScript==
+
+const HackerNewsCORS = document.body.appendChild(Object.assign(
+  document.createElement('div'),
+  { id: 'HackerNewsCORS' }
+));
+
+
+HackerNewsCORS.fetch = (url, options) => new Promise((resolve, reject) => {
+  const urlObj = new URL(url);
+  if (urlObj.hostname !== 'news.ycombinator.com') {
+    reject('Hostname must be news.ycombinator.com');
+    return;
+  }
+
+  GM.xmlHttpRequest({
+    ...options,
+    url,
+    onload: (response) => resolve(response),
+    onerror: (response) => resolve(response),
+    ontimeout: (response) => resolve(response),
+  });
+});
