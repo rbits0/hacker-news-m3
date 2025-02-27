5e3439ff89fc527028c6ac573079a1c93ce5393a
rbits0 <rbits0@gmail.com>
2025-02-27 12:54:17 +1100

    Add unfinished comments page

---
+++ app/(tabs)/(home)/comments/[id].tsx
@@ -0,0 +1,30 @@
+import Story from "@/components/Story";
+import { useGetItemByIdQuery } from "@/store/services/hackerNews";
+import { useLocalSearchParams } from "expo-router"
+import { View, StyleSheet } from "react-native";
+import { useTheme } from "react-native-paper";
+
+
+export default function CommentsScreen() {
+  const theme = useTheme();
+  const { id }: { id: string } = useLocalSearchParams();
+  const {
+    data: fetchedItem,
+    isLoading: itemIsLoading,
+    isError: itemIsError,
+  } = useGetItemByIdQuery(parseInt(id));
+
+  return (
+    <View style={[styles.container]}>
+      <Story itemId={parseInt(id)} />
+      {fetchedItem?.kids?.map(id => `${id}, `)}
+    </View>
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  }
+});
