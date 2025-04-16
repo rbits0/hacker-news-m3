10012b3fe59bbe2347a36a4d178af50e9f0a3f67
rbits0 <rbits0@gmail.com>
2025-04-16 20:04:50 +1000

    Add list of comments to comments page

--- app/(tabs)/(home,settings)/comments/[id].tsx
+++ app/(tabs)/(home,settings)/comments/[id].tsx
@@ -2,7 +2,12 @@ import Story from "@/components/Story";
 import { useGetItemByIdQuery } from "@/store/services/hackerNews";
 import { useLocalSearchParams } from "expo-router"
 import { View, StyleSheet } from "react-native";
-import { Text, useTheme } from "react-native-paper";
+import { useTheme } from "react-native-paper";
+import Comment from "@/components/Comment";
+import DynamicScrollList from "@/components/DynamicScrollList";
+
+
+const NUM_COMMENTS_PER_PAGE = 20;
 
 
 export default function CommentsScreen() {
@@ -14,10 +19,29 @@ export default function CommentsScreen() {
     isError: itemIsError,
   } = useGetItemByIdQuery(parseInt(id));
 
+  const data: (number | 'HEADER')[] = fetchedItem?.kids
+    ? ['HEADER', ...fetchedItem.kids]
+    : ['HEADER'];
+
   return (
     <View style={[styles.container, { backgroundColor: theme.colors.background }]}>
-      <Story itemId={parseInt(id)} showBody={true} disableCommentsLink={true} />
-      <Text>{fetchedItem?.kids?.map(id => `${id}, `)}</Text>
+
+      <DynamicScrollList
+        data={data}
+        renderItem={
+          ({ item }) => item === 'HEADER' ? (
+            <View style={styles.storyView}>
+              <Story itemId={parseInt(id)} showBody={true} disableCommentsLink={true} />
+            </View>
+          ) : (
+            <Comment itemId={item} />
+          )
+        }
+        itemsPerPage={NUM_COMMENTS_PER_PAGE}
+        contentContainerStyle={styles.innerList}
+        style={[styles.list]}
+      />
+
     </View>
   )
 }
@@ -27,5 +51,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-  }
+  },
+  list: {
+    width: '100%',
+    padding: 6,
+  },
+  innerList: {
+    gap: 10,
+  },
+  storyView: {
+    paddingBottom: 10,
+  },
 });
