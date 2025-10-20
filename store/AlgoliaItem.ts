205de5a02f56fc3f93b28a687324e71b447dbd72
rbits0 <rbits0@gmail.com>
2025-10-20 18:12:12 +1100

    Create function to convert AlgoliaItem to Item

--- store/AlgoliaItem.ts
+++ store/AlgoliaItem.ts
@@ -1,5 +1,7 @@
 // Item format from https://hn.algolia.com/api/v1/
 
+import Item from "./Item";
+
 export default interface AlgoliaItem {
   id: number,
   created_at: string,
@@ -10,4 +12,19 @@ export default interface AlgoliaItem {
   children: [AlgoliaItem],
   title?: string,
   url?: string,
+}
+
+
+export function algoliaItemToItem(item: AlgoliaItem): Item {
+  return {
+    id: item.id,
+    by: item.author,
+    time: Date.parse(item.created_at),
+    text: item.text ?? undefined,
+    parent: item.parent_id ?? undefined,
+    url: item.url,
+    score: item.points,
+    title: item.title,
+    descendants: item.children.length,
+  };
 }
