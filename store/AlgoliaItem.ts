45d9ae6c5381af9237c4d988d5930154346fa067
rbits0 <rbits0@gmail.com>
2025-10-20 17:53:38 +1100

    Fix indentation

--- store/AlgoliaItem.ts
+++ store/AlgoliaItem.ts
@@ -1,13 +1,13 @@
 // Item format from https://hn.algolia.com/api/v1/
 
 export default interface AlgoliaItem {
-    id: number,
-    created_at: string,
-    author: string,
-    text: string | null,
-    points: number,
-    parent_id: number | null,
-    children: [AlgoliaItem],
-    title?: string,
-    url?: string,
+  id: number,
+  created_at: string,
+  author: string,
+  text: string | null,
+  points: number,
+  parent_id: number | null,
+  children: [AlgoliaItem],
+  title?: string,
+  url?: string,
 }
